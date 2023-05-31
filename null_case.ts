eda55bc96d28de722b49711216e285c49c91026f
brucehuke <100748182+brucehuke@users.noreply.github.com>
2023-05-31 14:53:08 +0800

    Create null_case.ts

---
+++ null_case.ts
@@ -0,0 +1,13 @@
+let empty2: null = null
+
+if (empty2) {
+  console.log('~~~~~~~~~test1 if condition true')
+} else {
+  console.log('~~~~~~~~~test1 if condition false')
+}
+
+if (!empty2) {
+  console.log('~~~~~~~~~test2 if condition true')
+} else {
+  console.log('~~~~~~~~~test2 if condition false')
+}
