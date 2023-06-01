6badb46d1938f34c49d87d09bcca6e1e18f5a0e6
penghuizhen <penghuizhen@xiaomi.com>
2023-06-01 17:10:10 +0800

    add testcase

---
+++ class_export_extends.ts
@@ -0,0 +1,32 @@
+export class App {
+    _id: number = -1
+
+    constructor() {
+    }
+}
+
+
+// index
+export { App } from './framework/app'
+
+// app use 
+import { App } from './index'
+
+export default class Samples extends App {
+
+    onCreate(): void {
+        console.log('--- app created ---')
+    }
+
+    onShow(): void {
+        console.log('--- app show ---')
+    }
+
+    onHide(): void {
+        console.log('--- app hide ---')
+    }
+
+    onDestroy(): void {
+        console.log('--- app destroy ---')
+    }
+}
