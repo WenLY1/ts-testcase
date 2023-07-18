ca825fdd1ac3ef9ba98a5f7aae825b91b55f229e
wenlingyun1 <wenlingyun1@xiaomi.com>
2023-07-18 17:02:54 +0800

    add testcase-3.ts

---
+++ testcase-3.ts
@@ -0,0 +1,20 @@
+export function anyDivideAny(x: number, y: number) {
+    let a: any = x;
+    let b: any = y;
+    let c: any = a / b;
+    console.log(c);
+}
+
+export function anyMultiplyAny(x: number, y: number) {
+    let a: any = x;
+    let b: any = y;
+    let c: any = a * b;
+    console.log(c);
+}
+
+export function anyModAny(x: number, y: number) {
+    let a: any = x;
+    let b: any = y;
+    let c: any = a % b;
+    console.log(c);
+}
